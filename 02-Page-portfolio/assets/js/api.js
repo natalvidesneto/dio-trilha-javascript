a4f977cf1d1c21bc09406011f7afe3d8ba76084c
Natalvides Neto <natalvides.neto.01@gmail.com>
2025-09-30 16:31:08 -0300

    page-portfolio

---
+++ 02-Page-portfolio/assets/js/api.js
@@ -0,0 +1,6 @@
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/natalvidesneto/teste-portifolio/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
+}
